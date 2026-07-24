5421257f152c08145004b75f29f301ae1b52ddc8
Hiyab Teklu <205397659+hiyabteklu@users.noreply.github.com>
2026-07-25 00:05:31 +0300

    Enhance CoreConfig with site setup and default field
    Add default_auto_field and site configuration in CoreConfig.

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        try:
+            from django.contrib.sites.models import Site
+            Site.objects.update_or_create(
+                id=1,
+                defaults={
+                    'domain': 'wisdom-1-qp71.onrender.com',
+                    'name': 'Wisdom'
+                }
+            )
+        except Exception:
+            pass
